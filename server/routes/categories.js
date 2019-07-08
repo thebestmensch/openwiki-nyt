d78f96f41b42e90a7020db1f8fc19a1c95581813
JMensch <james@jamesmensch.com>
2019-07-08 16:54:12 -0500

    init

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,40 +1,49 @@
-'use strict'
+"use strict";
 
-const moment = require('moment')
+const moment = require("moment");
 
-const router = require('express-promise-router')()
+const router = require("express-promise-router")();
 
-const cache = require('../cache')
-const log = require('../logger')
-const {getMeta} = require('../list')
-const {fetchDoc, cleanName, fetchByline} = require('../docs')
-const {getTemplates, sortDocs, stringTemplate} = require('../utils')
-const {parseUrl} = require('../urlParser')
+const cache = require("../cache");
+const log = require("../logger");
+const { getMeta } = require("../list");
+const { fetchDoc, cleanName, fetchByline } = require("../docs");
+const { getTemplates, sortDocs, stringTemplate } = require("../utils");
+const { parseUrl } = require("../urlParser");
 
-router.get('*', handleCategory)
-module.exports = router
+router.get("*", handleCategory);
+module.exports = router;
 
-const categories = getTemplates('categories')
+const categories = getTemplates("categories");
 async function handleCategory(req, res) {
-  log.info(`GET ${req.path}`)
+  log.info(`GET ${req.path}`);
   // FIXME: consider putting this in middleware and save on req
-  const {meta, parent, data, root} = await parseUrl(req.path)
+  const { meta, parent, data, root } = await parseUrl(req.path);
 
-  if (!meta || !data) return 'next'
+  if (!meta || !data) return "next";
 
-  const {resourceType, tags, id} = meta
-  const {breadcrumb} = data
+  const { resourceType, tags, id } = meta;
+  const { breadcrumb } = data;
 
-  const layout = categories.has(root) ? root : 'default'
-  const template = `categories/${layout}`
+  const layout = categories.has(root) ? root : "default";
+  const template = `categories/${layout}`;
 
-  const parentMeta = getMeta(parent.id)
-  if (tags.includes('playlist') || (parentMeta && parentMeta.tags.includes('playlist'))) {
-    return 'next'
+  const parentMeta = getMeta(parent.id);
+  if (
+    tags.includes("playlist") ||
+    (parentMeta && parentMeta.tags.includes("playlist"))
+  ) {
+    return "next";
   }
 
   // don't try to fetch branch node
-  const contextData = prepareContextualData(data, req.path, breadcrumb, parent, meta.slug)
+  const contextData = prepareContextualData(
+    data,
+    req.path,
+    breadcrumb,
+    parent,
+    meta.slug
+  );
 
   const baseRenderData = Object.assign({}, contextData, {
     url: req.path,
@@ -42,76 +51,103 @@ async function handleCategory(req, res) {
     lastUpdatedBy: (meta.lastModifyingUser || {}).displayName,
     modifiedAt: meta.modifiedTime,
     createdAt: moment(meta.createdTime).fromNow(),
-    editLink: meta.mimeType === 'text/html' ? meta.folder.webViewLink : meta.webViewLink,
+    editLink:
+      meta.mimeType === "text/html"
+        ? meta.folder.webViewLink
+        : meta.webViewLink,
     id,
     template: stringTemplate
-  })
+  });
 
   // if this is a folder, just render from the generic data
-  if (resourceType === 'folder') {
+  if (resourceType === "folder") {
     return res.render(template, baseRenderData, (err, html) => {
-      if (err) throw err
+      if (err) throw err;
 
-      cache.add(id, meta.modifiedTime, req.path, html)
-      res.end(html)
-    })
+      cache.add(id, meta.modifiedTime, req.path, html);
+      res.end(html);
+    });
   }
 
   // for docs, fetch the html and then combine with the base data
-  const {html, originalRevision, sections} = await fetchDoc(id, resourceType, req)
-  res.locals.docId = data.id // we need this for history later
-  const revisionData = originalRevision.data || { lastModifyingUser: {} }
-  const payload = fetchByline(html, revisionData.lastModifyingUser.displayName)
-  res.render(template, Object.assign({}, baseRenderData, {
-    content: payload.html,
-    byline: payload.byline,
-    createdBy: revisionData.lastModifyingUser.displayName,
-    sections
-  }), (err, html) => {
-    if (err) throw err
-    cache.add(id, meta.modifiedTime, req.path, html)
-    res.end(html)
-  })
+  const { html, originalRevision, sections } = await fetchDoc(
+    id,
+    resourceType,
+    req
+  );
+  res.locals.docId = data.id; // we need this for history later
+  const revisionData = originalRevision.data || { lastModifyingUser: {} };
+  const payload = fetchByline(html, revisionData.lastModifyingUser.displayName);
+  res.render(
+    template,
+    Object.assign({}, baseRenderData, {
+      content: payload.html,
+      byline: payload.byline,
+      createdBy: revisionData.lastModifyingUser.displayName,
+      sections
+    }),
+    (err, html) => {
+      if (err) throw err;
+      cache.add(id, meta.modifiedTime, req.path, html);
+      res.end(html);
+    }
+  );
 }
 
 function prepareContextualData(data, url, breadcrumb, parent, slug) {
-  const breadcrumbInfo = breadcrumb.map(({id}) => getMeta(id))
+  const breadcrumbInfo = breadcrumb.map(({ id }) => getMeta(id));
 
-  const {children: siblings} = parent
-  const {children} = data
-  const self = url.split('/').slice(-1)[0]
+  const { children: siblings } = parent;
+  const { children } = data;
+  const self = url.split("/").slice(-1)[0];
   // most of what we are doing here is preparing parents and siblings
   // we need the url and parent object, as well as the breadcrumb to do that
-  const siblingLinks = createRelatedList(siblings, self, `${url.split('/').slice(0, -1).join('/')}`)
-  const childrenLinks = createRelatedList(children || {}, self, url)
+  const siblingLinks = createRelatedList(
+    siblings,
+    self,
+    `${url
+      .split("/")
+      .slice(0, -1)
+      .join("/")}`
+  );
+  const childrenLinks = createRelatedList(children || {}, self, url);
 
   // extend the breadcrumb with render data
   const parentLinks = url
-    .split('/')
+    .split("/")
     .slice(1, -1) // ignore the base empty string and self
     .map((segment, i, arr) => {
       return {
-        url: `/${arr.slice(0, i + 1).join('/')}`,
+        url: `/${arr.slice(0, i + 1).join("/")}`,
         name: cleanName(breadcrumbInfo[i].name),
         editLink: breadcrumbInfo[i].webViewLink
-      }
-    })
+      };
+    });
 
-  const {id, originalId} = breadcrumb.length ? breadcrumb[breadcrumb.length - 1] : data
+  const { id, originalId } = breadcrumb.length
+    ? breadcrumb[breadcrumb.length - 1]
+    : data;
   return {
     parentId: originalId || id,
     parentLinks,
     siblings: siblingLinks,
     children: childrenLinks
-  }
+  };
 }
 
 function createRelatedList(slugs, self, baseUrl) {
   return Object.keys(slugs)
-    .filter((slug) => slug !== self)
-    .map((slug) => {
-      const {id} = slugs[slug]
-      const {sort, prettyName, webViewLink, path: url, resourceType, tags} = getMeta(id)
+    .filter(slug => slug !== self)
+    .map(slug => {
+      const { id } = slugs[slug];
+      const {
+        sort,
+        prettyName,
+        webViewLink,
+        path: url,
+        resourceType,
+        tags
+      } = getMeta(id);
       return {
         sort,
         name: prettyName,
@@ -119,8 +155,8 @@ function createRelatedList(slugs, self, baseUrl) {
         resourceType,
         url,
         tags
-      }
+      };
     })
-    .filter(({tags}) => !tags.includes('hidden'))
-    .sort(sortDocs)
+    .filter(({ tags }) => !tags.includes("hidden"))
+    .sort(sortDocs);
 }
--- server/userAuth.js
+++ server/userAuth.js
@@ -1,91 +1,98 @@
-'use strict'
+"use strict";
 
-const passport = require('passport')
-const session = require('express-session')
-const md5 = require('md5')
-const GoogleStrategy = require('passport-google-oauth20')
+const passport = require("passport");
+const session = require("express-session");
+const md5 = require("md5");
+const GoogleStrategy = require("passport-google-oauth20");
 
-const log = require('./logger')
-const {stringTemplate: template} = require('./utils')
+const log = require("./logger");
+const { stringTemplate: template } = require("./utils");
 
-const router = require('express-promise-router')()
-const domains = new Set(process.env.APPROVED_DOMAINS.split(/,\s?/g))
+const router = require("express-promise-router")();
 
-passport.use(new GoogleStrategy.Strategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/redirect',
-  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-  passReqToCallback: true
-}, (request, accessToken, refreshToken, profile, done) => done(null, profile)))
+passport.use(
+  new GoogleStrategy.Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/redirect",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      passReqToCallback: true
+    },
+    (request, accessToken, refreshToken, profile, done) => done(null, profile)
+  )
+);
 
-router.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true
-}))
+router.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
-router.use(passport.initialize())
-router.use(passport.session())
+router.use(passport.initialize());
+router.use(passport.session());
 
 // seralize/deseralization methods for extracting user information from the
 // session cookie and adding it to the req.passport object
-passport.serializeUser((user, done) => done(null, user))
-passport.deserializeUser((obj, done) => done(null, obj))
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 
-router.get('/login', passport.authenticate('google', {
-  scope: [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile'
-  ],
-  prompt: 'select_account'
-}))
+router.get(
+  "/login",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile"
+    ],
+    prompt: "select_account"
+  })
+);
 
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-router.get('/auth/redirect', passport.authenticate('google'), (req, res) => {
-  res.redirect(req.session.authRedirect || '/')
-})
+router.get("/auth/redirect", passport.authenticate("google"), (req, res) => {
+  res.redirect(req.session.authRedirect || "/");
+});
 
 router.use((req, res, next) => {
-  const isDev = process.env.NODE_ENV === 'development'
-  const authenticated = req.isAuthenticated()
+  const isDev = process.env.NODE_ENV === "development";
+  const authenticated = req.isAuthenticated();
 
-  const passportUser = (req.session.passport || {}).user || {}
-  const [{value: userEmail = ''} = {}] = passportUser.emails || []
-  const [userDomain] = userEmail.split('@').slice(-1)
+  const passportUser = (req.session.passport || {}).user || {};
+  const [{ value: userEmail = "" } = {}] = passportUser.emails || [];
+  const [userDomain] = userEmail.split("@").slice(-1);
 
-  if (isDev || (authenticated && domains.has(userDomain))) {
-    setUserInfo(req)
-    return next()
+  if (isDev || authenticated) {
+    setUserInfo(req);
+    return next();
   }
 
-  if (authenticated && !domains.has(userDomain)) {
-    return next(Error('Unauthorized'))
-  }
-
-  log.info('User not authenticated')
-  req.session.authRedirect = req.path
-  res.redirect('/login')
-})
+  log.info("User not authenticated");
+  req.session.authRedirect = req.path;
+  res.redirect("/login");
+});
 
 function setUserInfo(req) {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     req.userInfo = {
-      email: process.env.TEST_EMAIL || template('footer.defaultEmail'),
-      userId: '10',
-      analyticsUserId: md5('10library')
-    }
-    return
+      email: process.env.TEST_EMAIL || template("footer.defaultEmail"),
+      userId: "10",
+      analyticsUserId: md5("10library")
+    };
+    return;
   }
-  req.userInfo = req.userInfo ? req.userInfo : {
-    email: req.session.passport.user.emails[0].value,
-    userId: req.session.passport.user.id,
-    analyticsUserId: md5(req.session.passport.user.id + 'library')
-  }
+  req.userInfo = req.userInfo
+    ? req.userInfo
+    : {
+        email: req.session.passport.user.emails[0].value,
+        userId: req.session.passport.user.id,
+        analyticsUserId: md5(req.session.passport.user.id + "library")
+      };
 }
 
-module.exports = router
+module.exports = router;
